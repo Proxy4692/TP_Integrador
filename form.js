fda82f0d1d4587bd3d92f9020dd323a0a1ba3637
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-05 17:27:22 -0300

    update

---
+++ form.js
@@ -0,0 +1,9 @@
+document.getElementById('formContacto').addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const nombre = document.getElementById('nombre').value
+    const telefono = document.getElementById('telefono').value
+    const email = document.getElementById('email').value
+
+
+})
