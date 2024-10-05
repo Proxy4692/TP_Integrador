a473dea5815ed5ef922038e6ee2de11ebc462dde
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-05 17:47:58 -0300

    form

--- form.js
+++ form.js
@@ -5,5 +5,35 @@ document.getElementById('formContacto').addEventListener('submit', function(even
     const telefono = document.getElementById('telefono').value
     const email = document.getElementById('email').value
 
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    const telefonoRegex = /^[0-9]{9}$/;
 
+    let errores = []
+
+    if(nombre.length === 0){
+        errores.push('El nombre es obligatorio.')
+    }
+
+    if(!emailRegex.test(email)){
+        errores.push('El correo electronico no es valido.')
+    }
+
+    if(!telefonoRegex.test(telefono)){
+        errores.push('El telefono debe tener 9 digitos.')
+    }
+
+    const resultadoDiv = document.getElementById('formResult')
+    resultadoDiv.innerHTML = ''
+
+    if(errores.length > 0){
+        errores.forEach(error => {
+            const errorParrafo = document.createElement('p')
+            errorParrafo.textContent = error
+            resultadoDiv.appendChild(errorParrafo)
+        })
+    }else{
+        const exitoParrafo = document.createElement('p')
+        exitoParrafo.textContent = 'Formulario enviado con exito. Nombre: ${nombre}, Correo: ${email}, Telefono: ${telefono}'
+        resultadoDiv.appendChild(exitoParrafo)
+    }
 })
