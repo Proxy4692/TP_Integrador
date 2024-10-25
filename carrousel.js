6a3d3253dbb664567451a17600933245a57c574b
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-25 16:55:17 -0300

    update

--- carrousel.js
+++ carrousel.js
@@ -1,11 +1,11 @@
 const images = [
-    {src:'https://cdn.cloudflare.steamstatic.com/steam/apps/632360/ss_2bb49071317f7b241a527cf6e7aabd2cb6af055b.600x338.jpg?t=1699992764',
+    {src:'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/632360/header.jpg?t=1728672963',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     },
-    {src: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/250900/ss_ffac64b3c52a148ed6100c8b0ab87d011274991c.600x338.jpg?t=1722458438',
+    {src: 'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/730/header.jpg?t=1729703045',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     },
-    {src: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1091500/ss_e5a94665dbfa5a30931cff2f45cdc0ebea9fcebb.600x338.jpg?t=1726188854',
+    {src: 'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/346110/header.jpg?t=1709879597',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     }
 ]
@@ -19,12 +19,10 @@ nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % images.length
     carrouselImage.src = images[currentIndex].src
 })
-
 prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length
     carrouselImage.src = images[currentIndex].src
 })
-
 carrouselImage.addEventListener('click', () => {
     window.location.href = images[currentIndex].link
 })
