17b3181df1dfab7147e9df28284076d7d509ffef
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-25 01:18:37 -0300

    1

--- carrousel.js
+++ carrousel.js
@@ -2,11 +2,9 @@ const images = [
     {src:'https://cdn.cloudflare.steamstatic.com/steam/apps/632360/ss_2bb49071317f7b241a527cf6e7aabd2cb6af055b.600x338.jpg?t=1699992764',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     },
-
     {src: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/250900/ss_ffac64b3c52a148ed6100c8b0ab87d011274991c.600x338.jpg?t=1722458438',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     },
-
     {src: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1091500/ss_e5a94665dbfa5a30931cff2f45cdc0ebea9fcebb.600x338.jpg?t=1726188854',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     }
@@ -19,12 +17,12 @@ const prevButton = document.getElementById('prev')
 
 nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % images.length
-    carrouselImage.src = images[currentIndex]
+    carrouselImage.src = images[currentIndex].src
 })
 
 prevButton.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length
-    carrouselImage.src = images[currentIndex]
+    carrouselImage.src = images[currentIndex].src
 })
 
 carrouselImage.addEventListener('click', () => {
