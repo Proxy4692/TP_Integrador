a20e86c0b6082c1902f20a767ebd0478f3973bf3
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-25 23:48:48 -0300

    more games

--- carrousel.js
+++ carrousel.js
@@ -7,6 +7,12 @@ const images = [
     },
     {src: 'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/346110/header.jpg?t=1709879597',
         link: 'https://proxy4692.github.io/vaporcito/articulo.html'
+    },
+    {src:'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1293830/header.jpg?t=1702576030',
+        link: 'https://proxy4692.github.io/vaporcito/articulo.html'
+    },
+    {src:'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/108600/header.jpg?t=1691508011',
+        link: 'https://proxy4692.github.io/vaporcito/articulo.html'
     }
 ]
 let currentIndex = 0
